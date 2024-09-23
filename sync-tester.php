8d89b84bc049b6a85727a34dc3023ef2c40b94e4
Navanath Bhosale <navanathb@bsf.io>
2024-09-23 10:46:27 +0530

    chore: version bump

--- sync-tester.php
+++ sync-tester.php
@@ -5,7 +5,7 @@
  * Description: A temporary plugin to test the syncing process with WP Pusher plugin.
  * Author: Skin Seoul
  * Author URI: skin-seoul.com/
- * Version: 0.0.1
+ * Version: 0.0.2
  * License: GPLv2 or later
  * Text Domain: skin-seoul
  *
@@ -23,6 +23,6 @@ define( 'SS_CORE_FILE', __FILE__ );
 define( 'SS_CORE_BASENAME', plugin_basename( SS_CORE_FILE ) );
 define( 'SS_CORE_DIR', plugin_dir_path( SS_CORE_FILE ) );
 define( 'SS_CORE_URL', plugins_url( '/', SS_CORE_FILE ) );
-define( 'SS_CORE_VER', '0.0.1' );
+define( 'SS_CORE_VER', '0.0.2' );
 define( 'SS_CORE_SLUG', 'ss-core' );
 
